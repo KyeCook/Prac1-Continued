6c38f0da7a3108bc7b514ea18e4786d2d24be5a4
KyeCook <acfanatics@hotmail.com>
2016-08-07 16:08:06 +1000

    Initial commit

---
+++ src/Hello_World.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ACfan on 2/08/2016.
+ */
+public class Hello_World {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/popup_dialogue_box.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by ACfan on 7/08/2016.
+ */
+public class popup_dialogue_box {
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "This is a dialogue box");
+    }
+}
